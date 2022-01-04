0799a9656204e3d554bafb5cdd5eb3afba4e382c
雷涛 <2534545316@qq.com>
2022-01-04 13:39:39 +0800

    leetcode 组合求和

---
+++ algorithm-example/src/main/java/com/canglang/algorithm/letcode/CombinationSum_39.java
@@ -0,0 +1,84 @@
+package com.canglang.algorithm.letcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author leitao.
+ * @time: 2021/12/31 0031-11:10
+ * @description:
+ * 给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
+ *
+ * candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。 
+ *
+ * 对于给定的输入，保证和为 target 的不同组合数少于 150 个。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：candidates = [2,3,6,7], target = 7
+ * 输出：[[2,2,3],[7]]
+ * 解释：
+ * 2 和 3 可以形成一组候选，2 + 2 + 3 = 7 。注意 2 可以使用多次。
+ * 7 也是一个候选， 7 = 7 。
+ * 仅有这两种组合。
+ * 示例 2：
+ *
+ * 输入: candidates = [2,3,5], target = 8
+ * 输出: [[2,2,2,2],[2,3,3],[3,5]]
+ * 示例 3：
+ *
+ * 输入: candidates = [2], target = 1
+ * 输出: []
+ * 示例 4：
+ *
+ * 输入: candidates = [1], target = 1
+ * 输出: [[1]]
+ * 示例 5：
+ *
+ * 输入: candidates = [1], target = 2
+ * 输出: [[1,1]]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= candidates.length <= 30
+ * 1 <= candidates[i] <= 200
+ * candidate 中的每个元素都 互不相同
+ * 1 <= target <= 500
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/combination-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class CombinationSum_39 {
+    public static void main(String[] args) {
+        System.out.println(combinationSum(new int[]{1, 2, 3}, 5));
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> combine = new ArrayList<Integer>();
+        dfs(candidates, target, ans, combine, 0);
+        return ans;
+    }
+
+    public static void dfs(int[] candidates, int target, List<List<Integer>> ans, List<Integer> combine, int idx) {
+        if (idx == candidates.length) {
+            return;
+        }
+        if (target == 0) {
+            ans.add(new ArrayList<Integer>(combine));
+            return;
+        }
+        // 直接跳过
+        dfs(candidates, target, ans, combine, idx + 1);
+        // 选择当前数
+        if (target - candidates[idx] >= 0) {
+            combine.add(candidates[idx]);
+            dfs(candidates, target - candidates[idx], ans, combine, idx);
+            combine.remove(combine.size() - 1);
+        }
+    }
+}
