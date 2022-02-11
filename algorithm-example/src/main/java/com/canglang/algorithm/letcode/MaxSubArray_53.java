e12bfd032d23c54aeaf0becc1f042f89c0773a0f
雷涛 <2534545316@qq.com>
2022-02-11 18:13:08 +0800

    53. 最大子数组和

--- algorithm-example/src/main/java/com/canglang/algorithm/letcode/MaxSubArray_53.java
+++ algorithm-example/src/main/java/com/canglang/algorithm/letcode/MaxSubArray_53.java
@@ -3,36 +3,35 @@ package com.canglang.algorithm.letcode;
 /**
  * @author leitao.
  * @time: 2022/2/11 0011-13:20
- * @description:
- * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
- *
+ * @description: 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * <p>
  * 子数组 是数组中的一个连续部分。
- *
+ * <p>
  *  
- *
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
  * 输出：6
  * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
  * 示例 2：
- *
+ * <p>
  * 输入：nums = [1]
  * 输出：1
  * 示例 3：
- *
+ * <p>
  * 输入：nums = [5,4,-1,7,8]
  * 输出：23
  *  
- *
+ * <p>
  * 提示：
- *
+ * <p>
  * 1 <= nums.length <= 105
  * -104 <= nums[i] <= 104
  *  
- *
+ * <p>
  * 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/maximum-subarray
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -40,12 +39,18 @@ package com.canglang.algorithm.letcode;
 public class MaxSubArray_53 {
 
     public static void main(String[] args) {
-        System.out.println(maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     }
 
     public static int maxSubArray(int[] nums) {
-
-        return 0;
+        //若当前指针所指元素之前的和小于0,则丢弃当前元素之前的数列
+        int maxSum = 0, curSum = 0;
+        //当前值  之前和  当前和  最大和
+        for (int i = 0; i < nums.length; i++) {
+            curSum = Math.max(nums[i], curSum + nums[i]);
+            maxSum = Math.max(curSum, maxSum);
+        }
+        return maxSum;
     }
 
 }
